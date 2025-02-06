1baa7ea4a343d7323f528942861d7aefc4e4f5f6
Kayode <bubykay@gmail.com>
2025-02-06 17:32:51 +0100

    added keepAlive

--- src/server.js
+++ src/server.js
@@ -1,6 +1,29 @@
-import app from './app.js'
-import config from './config/env.js'
+import app from "./app.js";
+import config from "./config/env.js";
+import https from "https";
+import cron from "node-cron";
+
+function keepAlive(url) {
+  https
+    .get(url, (res) => {
+      console.log(`Status: ${res.statusCode}`);
+    })
+    .on("error", (error) => {
+      console.error(`Error: ${error.message}`);
+    });
+}
+
+cron.schedule("*/5 * * * *", () => {
+  try {
+    keepAlive(
+      "https://hng-stage-1-wo7e.onrender.com/api/classify-number?number=56"
+    );
+    console.log("Pinging the server every 5 minutes");
+  } catch (error) {
+    console.error("Error during cron job execution:", error.message);
+  }
+});
 
 app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
+  console.log(`Server running on port ${config.port}`);
 });
